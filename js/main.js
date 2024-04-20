26096ca10aef9513b1ead6d257773464c85e6b57
Kevin Morales <moralesk765@gmail.com>
2024-04-20 22:36:35 +0800

    add mailto

--- js/main.js
+++ js/main.js
@@ -1,51 +1,51 @@
-// toggle icon navbar   
-let menuIcon = document.querySelector('#menu-icon')
-let navbar = document.querySelector('.navbar')
+// toggle icon navbar
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x')
-    navbar.classList.toggle('active')
-}
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
+// scroll sectios
 
-// scroll sectios   
-
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight; 
-        let id = sec.getAttribute('id')
+    if (top >= offset && top < offset + height) {
+      // active navbar links
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+        console.log(document.querySelector("header nav a[href*=" + id + "]"));
+      });
+      // active sections fro animtion on scroll
+      sec.classList.add("show-animate");
+    } else {
+      sec.classList.remove("show-animate");
+    }
+    // if want to use animation that repeats on scroll use this
+  });
 
-        if(top >= offset && top < offset + height){
-            // active navbar links
-            navLinks.forEach(links => {
-                links.classList.remove('active')
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
-                console.log(document.querySelector('header nav a[href*=' + id + ']'))
-            })
-            // active sections fro animtion on scroll 
-            sec.classList.add('show-animate');
-        }else{
-            sec.classList.remove('show-animate')
-        }
-        // if want to use animation that repeats on scroll use this
-    })
+  //sticky header
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-    //sticky header
-    let header  = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100);
+  // remove toggle icon and navbar when click navbar links(scroll)
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 
-    // remove toggle icon and navbar when click navbar links(scroll)
-    menuIcon.classList.remove('bx-x')
-    navbar.classList.remove('active');
+  // animation footer on scroll
+  // let footer = document.querySelector('footer');
 
-    // animation footer on scroll
-    let footer = document.querySelector('footer');
-
-    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight)
-}
+  // footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight)
+};
